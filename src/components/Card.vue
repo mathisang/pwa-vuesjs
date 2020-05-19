a938790cdffeb9a38d61ca2e81bfe76cce3dc3a0
TimBlanchard <timothee.blanchard37@gmail.com>
2020-05-19 13:38:08 +0200

    start card

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<!--
+<template>
+  <div @mouseover="overCard" class="card-container">
+    {{ title }}
+    <div class="info-container" v-on:mouseover="overCard" v-on:mouseleave="overCard">
+bem
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Card',
+        data() {
+            return {
+                showCard: false
+            }
+        },
+        props: {
+            title: String
+        },
+        methods : {
+            overCard: function() {
+                this.showCard = !this.showCard;
+            }
+        }
+    }
+</script>
+<style scoped>
+  .card-container {
+    background-image: url("../assets/img/peaky.png");
+    height: 380px;
+    width: 300px;
+    background-size: cover;
+    background-position: center;
+  }
+  .card-container:hover {
+    background-image: url("../assets/img/peaky.png");
+    height: 380px;
+    width: 300px;
+    background-size: cover;
+    background-position: center;
+  }
+
+</style>
+-->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld @updateMessage="title = $event" :msg="title"/>
+    <!--<Card title="titre"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+/*import Card from '@/components/Card.vue'*/
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld/*,
+      Card*/
   },
   data() {
     return {
