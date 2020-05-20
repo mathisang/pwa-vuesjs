2f4a1bb1dba94e7183c05e9c0d6836d7d19ad6c0
Mathis Angibaud <m.angibaudmathis@gmail.com>
2020-05-20 14:14:06 +0200

    Setting PWA

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     pwa: {
-        name: 'Mon blog',
-        themeColor: '#d28c79',
+        name: 'Baz\'art',
+        themeColor: '#FFD800',
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
@@ -11,6 +11,48 @@ module.exports = {
         workboxPluginMode: 'InjectManifest',
         workboxOptions : {
             swSrc: 'src/service-worker.js'
-        }
+        },
+        icons: [
+            {
+                src: "img/icons/icon-72x72.png",
+                sizes: "72x72",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-96x96.png",
+                sizes: "96x96",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-128x128.png",
+                sizes: "128x128",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-144x144.png",
+                sizes: "144x144",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-152x152.png",
+                sizes: "152x152",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-192x192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-384x384.png",
+                sizes: "384x384",
+                type: "image/png"
+            },
+            {
+                "src": "img/icons/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
+        ],
     }
 }
